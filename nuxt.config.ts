68287efc75772650994ad9448626f134ca5c1787
Felix <felixdenoix@users.noreply.github.com>
2022-11-10 10:10:27 +0000

    update with claudebarde proposal

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,34 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
+import path from 'path'
+
 export default defineNuxtConfig({
 
   ssr: false,
+
+  vite: {
+    define: {
+      Buffer: Buffer,
+      global: 'globalThis',
+    },
+    build: {
+      target: "esnext",
+      commonjsOptions: {
+        transformMixedEsModules: true
+      }
+    },
+    resolve: {
+      alias: {
+        // dedupe @airgap/beacon-sdk
+        // I almost have no idea why it needs `cjs` on dev and `esm` on build, but this is how it works 🤷‍♂️
+        "@airgap/beacon-sdk": path.resolve(
+          path.resolve(),
+          `./node_modules/@airgap/beacon-sdk/dist/esm/index.js`
+        ),
+        // polyfills
+        "readable-stream": "vite-compatible-readable-stream",
+        stream: "vite-compatible-readable-stream"
+      }
+    }
+  }
 
 })
