40d669c8646e93696acb009a206a6e93bf1f05b2
Felix DENOIX <felix.denoix@gmail.com>
2022-11-10 01:27:34 +0100

    init

---
+++ app.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>Wallet Login</div>
+  <button @click="handleLogin">Wallet login</button>
+</template>
+
+<script setup lang="ts">
+import { TezosToolkit } from '@taquito/taquito';
+import { BeaconWallet } from '@taquito/beacon-wallet';
+
+const Tezos = new TezosToolkit('https://mainnet-tezos.giganode.io');
+const wallet = new BeaconWallet({ name: 'Beacon Docs Taquito' });
+Tezos.setWalletProvider(wallet);
+
+const handleLogin = async () => {
+  try {
+    console.log('Requesting permissions...');
+    const permissions = await wallet.client.requestPermissions();
+    console.log('Got permissions:', permissions.address);
+  } catch (error) {
+    console.log('Got error:', error);
+  }
+};
+
+</script>
